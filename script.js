85fa52fa4f2f7775da56faa6a198d82952994dea
McCambley <jakemccamb16@gmail.com>
2023-11-19 21:00:55 -0500

    Refactor code and add page navigation

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // @ts-check
+// --- Page Navigation ---
+const pageButtons = document.querySelectorAll(".page-button");
+const pages = document.querySelectorAll(".page");
+const calculatorPageButton = document.querySelector("#calculator-page-button");
+const builderPagebutton = document.querySelector("#builder-page-button");
+const calculatorPage = document.querySelector("#calculator-page");
+const builderPage = document.querySelector("#builder-page");
+
+pageButtons.forEach((button) => {
+  button.addEventListener("click", togglePage);
+});
+
+function togglePage() {
+  const calculatorIsActivePage = calculatorPage.classList.contains("active");
+  if (calculatorIsActivePage) {
+    calculatorPage.classList.remove("active");
+    builderPage.classList.add("active");
+    calculatorPageButton.classList.remove("active");
+    builderPagebutton.classList.add("active");
+  } else {
+    calculatorPage.classList.add("active");
+    builderPage.classList.remove("active");
+    calculatorPageButton.classList.add("active");
+    builderPagebutton.classList.remove("active");
+  }
+}
+
+// --- Plate Calculator ---
 const calculatorForm = document.querySelector(".form");
 const barInputs = document.querySelectorAll("input[name='options']");
 const allInputs = document.querySelectorAll("input");
@@ -79,3 +107,59 @@ function calculate(event) {
     }
   });
 }
+
+// --- Bar Builder ---
+
+let totalWeight = 0;
+let weights = {};
+
+function addPlate(weight) {
+  const barWeight = parseInt(document.getElementById("barWeight").value);
+  weights[weight] = weights[weight] ? weights[weight] + 1 : 1;
+  const addedWeight = Object.keys(weights)
+    .map((key) => {
+      return parseInt(key) * weights[key];
+    })
+    .reduce((a, b) => a + b * 2, 0);
+
+  console.log(addedWeight, weights);
+
+  totalWeight = barWeight + addedWeight;
+  document.getElementById("totalWeight").textContent = totalWeight;
+  updateCurrentPlates();
+}
+
+function clearPlates() {
+  initializeCalculator();
+}
+
+function initializeCalculator() {
+  const barWeight = parseInt(document.getElementById("barWeight").value);
+  totalWeight = barWeight;
+  document.getElementById("totalWeight").textContent = totalWeight;
+  weights = {};
+  updateCurrentPlates();
+}
+
+// Update total weight when bar weight is changed
+document.getElementById("barWeight").addEventListener("change", () => {
+  totalWeight = parseInt(document.getElementById("barWeight").value);
+  weights = {};
+  document.getElementById("totalWeight").textContent = totalWeight;
+  updateCurrentPlates();
+});
+
+function updateCurrentPlates() {
+  const currentPlates = document.querySelector(".current-plates");
+  if (Object.keys(weights).length === 0) {
+    currentPlates.textContent = "---";
+    return;
+  }
+  currentPlates.textContent = Object.keys(weights)
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .reduce((acc, key) => {
+      return acc + `${key}x${weights[key]} `;
+    }, "");
+}
+
+initializeCalculator();
